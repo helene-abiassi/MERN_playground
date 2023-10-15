b2a1322d8fcffaad3d858e5804b33a53d9672abc
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-15 20:07:54 +0200

    Delete server/routes/personRoutes.js

--- server/routes/personRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllPeople } from "../controller/personController.js";
-
-const router = express.Router();
-
-router.get("/all", getAllPeople);
-
-export default router;
