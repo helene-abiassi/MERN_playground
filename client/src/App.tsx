5a66e1403385347f0176eeb5de4063f0f427e1cf
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-18 14:14:41 +0200

    set up my react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,61 @@
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
 import "../src/styles/main.css";
-import Footer from "./compoents/Footer";
-import NavBar from "./compoents/NavBar";
-import SignUp from "./compoents/SignUp";
+import Footer from "./components/Footer";
+// import NavBar from "./components/NavBar";
+import SignUp from "./views/SignUp";
+import LogIn from "./views/LogIn";
+import About from "./views/About";
+import Home from "./views/Home";
+import NavBar from "./components/NavBar";
+import Experiences from "./views/Experiences";
+import Map from "./views/Map";
+import ErrorPage from "./views/ErrorPage";
+import ExperienceDetails from "./views/ExperienceDetails";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
+        <Route index element={<Home />} />
+        <Route path="experiences" element={<Experiences />} />
+        <Route
+          path="experiences/:experienceTitle"
+          element={<ExperienceDetails />}
+        />
+        <Route path="experiences/:type" element={<Experiences />} />
+        //!Fix this to be connected to my array expTypes
+        <Route path="map" element={<Map />} />
+        <Route path="about" element={<About />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={<LogIn />} />
+      </Route>
+    )
+  );
+
   return (
     <>
       <div className="mainBody">
-        <NavBar />
-        <h1>explore. discover. share.</h1>
-        <SignUp />
+        <RouterProvider router={router} />
+        <Outlet />
         <Footer />
       </div>
     </>
   );
 }
+
+const Root = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
 
 export default App;
