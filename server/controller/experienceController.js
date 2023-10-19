3de95b2d017b4e8778b18832fb2f43f8b8380198
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-19 18:03:59 +0200

    set up my url params

--- server/controller/experienceController.js
+++ server/controller/experienceController.js
@@ -1,9 +1,20 @@
+import { commentModel } from "../models/commentModel.js";
 import { experienceModel } from "../models/experienceModel.js";
 
 const getAllExperiences = async (req, res) => {
-  const allExperiences = await experienceModel.find();
+  const allExperiences = await experienceModel.find().populate([
+    {
+      path: "bookmarked_by",
+      select: ["username", "bio", "member_since", "user_image"],
+    },
+    { path: "comments" },
+    {
+      path: "user",
+      select: ["username", "bio", "member_since", "user_image"],
+    },
+  ]);
 
-  console.log("allExperiences :>> ", allExperiences);
+  // console.log("allExperiences :>> ", allExperiences);
 
   res.json({
     number: allExperiences.length,
@@ -11,4 +22,34 @@ const getAllExperiences = async (req, res) => {
   });
 };
 
-export { getAllExperiences };
+
+const getExperiencesByType = async (req, res) => {
+
+  const { experienceType } = req.params;
+
+  try {
+    const experiences = await experienceModel.find({
+      experienceType: experienceType,
+    });
+
+    if (experiences.length > 0) {
+      res.status(200).json({
+        number: experiences.length,
+        data: experiences,
+      });
+    } else {
+      res.status(200).json({
+        number: experiences.length,
+        errorMessage: "OH NO! No such type exists",
+      });
+    }
+  } catch (error) {
+    console.log("expType error :>> ", error);
+    res.status(500).json({
+      errorMessage: "something went wrong in the request",
+      error,
+    });
+  }
+};
+
+export { getAllExperiences, getExperiencesByType };
