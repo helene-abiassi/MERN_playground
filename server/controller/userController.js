77025cb31ba24c8183b789c3f6ecaaf5fce8b86e
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-19 23:24:42 +0200

    populate users

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,15 +1,27 @@
 import { v2 as cloudinary } from "cloudinary";
+import { userModel } from "../models/userModel.js";
+import { experienceModel } from "../models/experienceModel.js";
+import { hashPassword } from "../utilities/hashPassword.js";
 
-// const getAllUsers = async (req, res) => {
-//   const allUsers = await userModel.find();
+const getAllUsers = async (req, res) => {
+  const allUsers = await userModel.find().populate([
+    {
+      path: "bookmarks",
+      select: ["title", "publication_date", "photo"],
+    },
+    {
+      path: "submissions",
+      select: ["title", "publication_date", "photo"],
+    },
+  ]);
 
-//   console.log("allUsers :>> ", allUsers);
+  console.log("allUsers :>> ", allUsers);
 
-//   res.json({
-//     number: allUsers.length,
-//     data: allUsers,
-//   });
-// };
+  res.json({
+    number: allUsers.length,
+    data: allUsers,
+  });
+};
 
 const uploadImage = async (req, res) => {
   console.log(req.file);
@@ -37,4 +49,61 @@ const uploadImage = async (req, res) => {
   }
 };
 
-export { uploadImage };
+const signUp = async (req, res) => {
+  // receive all the new user information in the body of the req coming from client
+  // it will process that info and store it in db
+  console.log("req.body :>> ", req.body);
+
+  // Hash user password
+
+  try {
+    const hashedPassword = await hashPassword(req.body.password);
+    console.log("hashedPassword :>> ", hashedPassword);
+
+    if (hashedPassword) {
+      // check if user already exists
+      const existingUser = await userModel.findOne({ email: req.body.email });
+
+      if (existingUser) {
+        res.status(200).json({
+          message: "email already exists in the db",
+        });
+      } else {
+        // if no existing user, we save the new user
+
+        try {
+          const newUser = new userModel({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+            user_image: req.body.user_image,
+          });
+
+          console.log("newUser :>> ", newUser);
+
+          const savedUser = await newUser.save();
+          res.status(201).json({
+            message: "New user registered",
+            user: {
+              userName: savedUser.username,
+              email: savedUser.email,
+              userImage: savedUser.user_image,
+            },
+          });
+        } catch (error) {
+          console.log("error saving user :>> ", error);
+          res.status(500).json({
+            message: "something went wrong when registering your user",
+          });
+        }
+      }
+    }
+  } catch (error) {
+    console.log("error :>> ", error);
+    res.status(500).json({
+      message: "Oh no, it went wrong!",
+    });
+  }
+};
+
+export { uploadImage, signUp, getAllUsers };
