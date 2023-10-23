b56d96b2b80c1b996b79b8c3c0c6157c4657019b
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-23 22:51:39 +0200

    added routes

--- server/routes/experienceRoutes.js
+++ server/routes/experienceRoutes.js
@@ -1,8 +1,26 @@
 import express from "express";
-import { getAllExperiences } from "../controller/experienceController.js";
+import {
+  getAllExperiences,
+  getExperiencesByCity,
+  getExperiencesByCountry,
+  getExperiencesById,
+  getExperiencesByType,
+  submitExperience,
+} from "../controller/experienceController.js";
 
 const router = express.Router();
 
+//GET Routes
 router.get("/all", getAllExperiences);
+router.get("/:country", getExperiencesByCountry);
+router.get("/:country/:city", getExperiencesByCity);
+router.get("/type/:experienceType", getExperiencesByType);
+router.get("/id/:_id", getExperiencesById);
+
+//POST Routes
+router.post("/experiencesubmission", submitExperience);
+
+//Routes to set up :
+//&& Query params: bookmarks.length, comments.length??
 
 export default router;
