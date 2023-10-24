404ec886957855078d0c67e8b83dd418a88be76b
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-25 01:02:56 +0200

    Update experienceController.js

--- server/controller/experienceController.js
+++ server/controller/experienceController.js
@@ -1,7 +1,11 @@
+import { v2 as cloudinary } from "cloudinary";
 import { commentModel } from "../models/commentModel.js";
 import { experienceModel } from "../models/experienceModel.js";
+import { userModel } from "../models/userModel.js";
 
 const getAllExperiences = async (req, res) => {
+  const { comments } = req.query;
+
   const allExperiences = await experienceModel.find().populate([
     {
       path: "bookmarked_by",
@@ -9,11 +13,10 @@ const getAllExperiences = async (req, res) => {
     },
     { path: "comments" },
     {
-      path: "user",
-      select: ["username", "bio", "member_since", "user_image"],
+      path: "author",
+      select: ["username", "email", "bio", "member_since", "user_image"],
     },
   ]);
-
   // console.log("allExperiences :>> ", allExperiences);
 
   res.json({
@@ -22,8 +25,36 @@ const getAllExperiences = async (req, res) => {
   });
 };
 
-const getExperiencesByType = async (req, res) => {
+const getExperiencesById = async (req, res) => {
+  const id = req.params._id;
 
+  try {
+    const experienceByID = await experienceModel.find({
+      _id: id,
+    });
+    console.log("experienceByID :>> ", experienceByID);
+
+    if (experienceByID.length > 0) {
+      res.status(200).json({
+        number: experienceByID.length,
+        data: experienceByID,
+      });
+    } else {
+      res.status(200).json({
+        number: experienceByID.length,
+        errorMessage: "OH NO! No such id exists",
+      });
+    }
+  } catch (error) {
+    console.log("expType error :>> ", error);
+    res.status(500).json({
+      errorMessage: "something went wrong in the request",
+      error,
+    });
+  }
+};
+
+const getExperiencesByType = async (req, res) => {
   const { experienceType } = req.params;
 
   try {
@@ -51,4 +82,149 @@ const getExperiencesByType = async (req, res) => {
   }
 };
 
-export { getAllExperiences, getExperiencesByType };
+const getExperiencesByCountry = async (req, res) => {
+  const { country } = req.params;
+  console.log("country :>> ", country);
+
+  try {
+    const experienceByCountry = await experienceModel.find({
+      "location.country": country,
+    });
+    console.log("experienceByCountry :>> ", experienceByCountry);
+
+    if (experienceByCountry.length > 0) {
+      res.status(200).json({
+        number: experienceByCountry.length,
+        data: experienceByCountry,
+      });
+    } else {
+      res.status(200).json({
+        number: experienceByCountry.length,
+        errorMessage: "OH NO! No such country exists",
+      });
+    }
+  } catch (error) {
+    console.log("expType error :>> ", error);
+    res.status(500).json({
+      errorMessage: "something went wrong in the request",
+      error,
+    });
+  }
+};
+
+const getExperiencesByCity = async (req, res) => {
+  const { country, city } = req.params;
+  console.log("city :>> ", city);
+
+  try {
+    const experienceByCity = await experienceModel.find({
+      "location.city": city,
+      "location.country": country,
+    });
+    console.log("experienceByCity :>> ", experienceByCity);
+
+    if (experienceByCity.length > 0) {
+      res.status(200).json({
+        number: experienceByCity.length,
+        data: experienceByCity,
+      });
+    } else {
+      res.status(200).json({
+        number: experienceByCity.length,
+        errorMessage: "OH NO! No such country/ city exists",
+      });
+    }
+  } catch (error) {
+    console.log("expType error :>> ", error);
+    res.status(500).json({
+      errorMessage: "something went wrong in the request",
+      error,
+    });
+  }
+};
+
+const submitExperience = async (req, res) => {
+  console.log("req.body :>> ", req.body);
+
+  try {
+    const existingUser = await userModel.findOne({ email: req.body.email });
+    console.log("existingUser :>> ", existingUser);
+    if (existingUser) {
+      try {
+        const newSubmission = new experienceModel({
+          author: existingUser._id,
+          title: req.body.title,
+          caption: req.body.caption,
+          photo: req.body.photo,
+          location: {
+            country: req.body.country,
+            city: req.body.city,
+            longitude: req.body.longitude,
+            latitude: req.body.latitude,
+          },
+          experienceType: req.body.experienceType,
+          text_body: req.body.text_body,
+          photo_body: req.body.photo_body,
+        });
+
+        const savedSubmission = await newSubmission.save();
+
+        console.log("savedSubmission :>> ", savedSubmission);
+        res.status(201).json({
+          message: "Experience posted successfully",
+          submission: savedSubmission,
+        });
+      } catch (error) {
+        console.log("error when trying to submit an experience: ", error);
+        res.status(500).json({
+          message: "Something went wrong when trying to post an experience",
+        });
+      }
+    } else {
+      res.status(401).json({
+        message: "You need to be logged in to submit an experience",
+      });
+    }
+  } catch (error) {
+    console.log("Catch error: ", error);
+    res.status(500).json({
+      message: "Oh no! Something went wrong!",
+    });
+  }
+};
+
+const uploadPhoto = async (req, res) => {
+  console.log(req.file);
+
+  //Upload the file to cloudinary /if/ there is a file in req
+  if (req.file) {
+    try {
+      // Upload the image
+      const uploadedImage = await cloudinary.uploader.upload(req.file.path, {
+        folder: "voyageApp/userphotos",
+      });
+      console.log("uploadedImage", uploadedImage);
+      res.status(200).json({
+        message: "Image uploaded successfully",
+        userImage: uploadedImage.secure_url,
+      });
+      // Save the photo in the userphoto collection
+    } catch (error) {
+      console.error("error", error);
+    }
+  } else {
+    res.status(5000).json({
+      error: "File type not supported",
+    });
+  }
+};
+
+export {
+  getAllExperiences,
+  getExperiencesByType,
+  getExperiencesById,
+  getExperiencesByCountry,
+  getExperiencesByCity,
+  submitExperience,
+  uploadPhoto,
+};
