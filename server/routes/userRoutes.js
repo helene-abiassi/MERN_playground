12a26872d6c3d326231cbbad4a9a7a148f97ef1e
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-25 01:05:51 +0200

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,25 @@
 import express from "express";
-import { getAllUsers } from "../controller/userController";
+import {
+  getAllUsers,
+  getUserById,
+  signUp,
+  uploadImage,
+} from "../controller/userController.js";
+import { multerUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.get("/users", getAllUsers);
+//GET Routes
+router.get("/all", getAllUsers);
+router.get("/id/:_id", getUserById);
+// router.get("/bookmarks/:_id", getUserBookmarks);
+// router.get("/id/:_id", getUserSubmissions);
+
+//POST Routes
+router.post("/imageUpload", multerUpload.single("userImage"), uploadImage);
+router.post("/signup", signUp);
+
+//Posts routes:
+// URL param: login
 
 export default router;
