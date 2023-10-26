c4833bd897e1c157066ad3aae78cd09fe601fd79
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-26 21:48:54 +0200

    Update About.tsx

--- client/src/views/About.tsx
+++ client/src/views/About.tsx
@@ -1,7 +1,35 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 function About() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <h1>about</h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div className="beigeBox">
+          <p>
+            Lorem ipsibunventore, amet natus reiciendis ipsam quasi fugiat at ut
+            soluta consequatur et magnam illum corporis. Quaerat blanditiis
+            voluptatem, adipisci numquam autem at, repellat praesentium earum
+            consequatur asperiores perspiciatis animi rerum? A suscipit esse
+            beatae. Nisi quod numquam accusantium omnis veniam dicta velit,
+            labore cum excepturi animi, possimus quis doloremque, porro.
+          </p>
+          <br />
+          <Link className="nakdButton" to={"/experiences"}>
+            join our community
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
