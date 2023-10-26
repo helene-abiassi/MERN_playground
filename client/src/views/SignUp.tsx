9c46e54ac6186701ab7c7d409d308ab9791c3374
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-26 21:50:40 +0200

    Update SignUp.tsx

--- client/src/views/SignUp.tsx
+++ client/src/views/SignUp.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
+import Signup from "../components/Signup";
+
 function SignUp() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <h1>sign up</h1>
-      <SignUp />
+      <Signup />
     </div>
   );
 }
