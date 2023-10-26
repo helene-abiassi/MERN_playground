faed0b06bd24fe16c88cc9d6a52f04517d75959c
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-26 21:49:44 +0200

    Update Home.tsx

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,7 +1,40 @@
+import "../styles/Home.css";
+import { Link } from "react-router-dom";
+import Carousel from "../components/Carousel";
+import { useEffect } from "react";
+
 function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <h1>explore. discover. share.</h1>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div className="beigeBox">
+          <h2 className="subHeader">Sub Header</h2>
+          <p>
+            Lorem ipsibunventore, amet natus reiciendis ipsam quasi fugiat at ut
+            soluta consequatur et magnam illum corporis. Quaerat blanditiis
+            voluptatem, adipisci numquam autem at.
+          </p>
+          <br />
+          <Link className="nakdButton" to={"/experiences"}>
+            start exploring
+          </Link>
+        </div>
+      </div>
+
+      <br />
+      <div className="top5Carousel">
+        <Carousel />
+      </div>
     </div>
   );
 }
