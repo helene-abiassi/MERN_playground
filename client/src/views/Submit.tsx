14d23891b0e0bf525f3d29aaa0ccbc2f9b0c391b
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-26 21:52:10 +0200

    Add files via upload

---
+++ client/src/views/Submit.tsx
@@ -0,0 +1,12 @@
+import SubmitExperience from "../components/submitExperience";
+
+function Submit() {
+  return (
+    <div>
+      <h1>submit your story</h1>
+      <SubmitExperience />
+    </div>
+  );
+}
+
+export default Submit;
