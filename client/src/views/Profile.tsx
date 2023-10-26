6014f7828f85fb0574ad290420443fefa04e7a8c
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-26 21:50:25 +0200

    Update Profile.tsx

--- client/src/views/Profile.tsx
+++ client/src/views/Profile.tsx
@@ -1,7 +1,34 @@
-function Profile(props) {
+import { useEffect } from "react";
+import BucketList from "../components/BucketList";
+import ProfileCard from "../components/ProfileCard";
+import "../styles/ProfilePage.css";
+
+function Profile() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <h1>profile</h1>
+      <div className="profileBox">
+        <div className="titleEditIcon editIcon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M20.71 7.04C21.1 6.65 21.1 6 20.71 5.63L18.37 3.29C18 2.9 17.35 2.9 16.96 3.29L15.12 5.12L18.87 8.87L20.71 7.04ZM3 17.25V21H6.75L17.81 9.93L14.06 6.18L3 17.25Z"
+              fill="black"
+            />
+          </svg>
+        </div>
+        <ProfileCard />
+        <BucketList />
+      </div>
     </div>
   );
 }
