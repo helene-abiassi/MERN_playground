18c16804e9135269edabf10554888379d209cb65
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-29 17:49:05 +0100

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,25 +1,26 @@
 import express from "express";
 import {
   getAllUsers,
+  getProfile,
   getUserById,
+  logIn,
   signUp,
   uploadImage,
 } from "../controller/userController.js";
 import { multerUpload } from "../middlewares/multer.js";
+import jwtAuth from "../middlewares/jwtAuth.js";
 
 const router = express.Router();
 
 //GET Routes
 router.get("/all", getAllUsers);
 router.get("/id/:_id", getUserById);
-// router.get("/bookmarks/:_id", getUserBookmarks);
-// router.get("/id/:_id", getUserSubmissions);
+router.get("/profile", jwtAuth, getProfile);
+//Add all the POST routes that need jwt
 
 //POST Routes
-router.post("/imageUpload", multerUpload.single("userImage"), uploadImage);
+router.post("/imageUpload", multerUpload.single("user_image"), uploadImage);
 router.post("/signup", signUp);
-
-//Posts routes:
-// URL param: login
+router.post("/login", logIn);
 
 export default router;
