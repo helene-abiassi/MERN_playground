bb8f6f07e869957673c7edb047aa555c6b465c36
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-29 17:26:45 +0100

    Update ExpCard.tsx

--- client/src/components/ExpCard.tsx
+++ client/src/components/ExpCard.tsx
@@ -1,14 +1,28 @@
-function ExpCards() {
+import { Link } from "react-router-dom";
+import { Experience } from "../types/customTypes";
+import { useEffect } from "react";
+import { formatDate } from "./Functions";
+
+export interface ExperienceCardProp {
+  experience: Experience | Experience[];
+}
+
+function ExpCards({ experience }: ExperienceCardProp) {
+  const { _id, author, title, publication_date, photo } =
+    experience as Experience;
+
+  useEffect(() => {}, [experience]);
+
   return (
     <div>
-      <h1>my story</h1>
-      <img
-        style={{ width: "30%", height: "30%" }}
-        src="https://res.cloudinary.com/dfm1r4ikr/image/upload/v1697311768/voyageApp/expphoto_treptowerPark_hrlyk7.png"
-        alt=""
-      />
-      <figcaption>Caption of the photo</figcaption>
-      {/* <Link to={`${experience.title}`}>view more</Link> */}
+      <img src={photo} style={{ width: "20%" }} />
+      <h4>{title}</h4>
+      <p>
+        written by {author.username}, {formatDate(publication_date)}
+      </p>
+      <Link to={`${title}`} state={{ experience: experience }}>
+        View More!!
+      </Link>
     </div>
   );
 }
