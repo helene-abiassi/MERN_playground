29b27c8906f6def2fc0012a177e37b365c84876f
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-30 17:48:39 +0100

    added Auth context

--- client/src/components/ProfileCard.tsx
+++ client/src/components/ProfileCard.tsx
@@ -1,56 +1,21 @@
-import { useEffect, useState } from "react";
-import { User } from "../types/customTypes";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { formatDate } from "./Functions";
 
 function ProfileCard() {
-  const [user, setUser] = useState<User | null>(null);
+  const { user, getProfile, logOut } = useContext(AuthContext);
 
-  const getProfile = async () => {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
-      alert("you need to log in first!");
-      //Redirect user to login/ protected route page
-    }
-    if (token) {
-      const myHeaders = new Headers();
-      myHeaders.append("Authorization", `Bearer ${token}`);
-
-      const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-      };
-
-      try {
-        const response = await fetch(
-          "http://localhost:5005/api/users/profile",
-          requestOptions
-        );
-
-        if (!response.ok) {
-          alert(response.statusText);
-        }
-
-        if (response.ok) {
-          const result = await response.json();
-          console.log("result to get my user profile :>> ", result);
-          const user = result.userProfile as User;
-
-          setUser(user);
-        }
-      } catch (err) {
-        const error = err as Error;
-        console.log("error :>> ", error.message);
-      }
-    }
-  };
 
   useEffect(() => {
+    console.log("user :>> ", user);
+
     getProfile();
-  }, []);
+  }, [user]);
 
   return (
     <div>
       <div className="profileColumns">
+        <button onClick={logOut}>log out</button>
         <div className="profileColumnLeft">
           <p className="inputKeys">photo:</p>
           <img style={{ width: "50%" }} src={user?.user_image} alt="" />{" "}
@@ -58,11 +23,11 @@ function ProfileCard() {
         <div className="profileColumn">
           <p className="inputKeys">username:{user?.username}</p>
           <p className="inputKeys">email:{user?.email}</p>
-          <p className="inputKeys">member since:{user?.member_since}</p>{" "}
-          {/* //!FORMAT DATE */}
+          <p className="inputKeys">
+            member since:{formatDate(user?.member_since)}
+          </p>
           <p className="inputKeys">bio:{user?.bio}</p>
         </div>
-        {/* <button onClick={getProfile}>get profile</button> */}
       </div>
     </div>
   );
