14afd067165fb5f700582febfb791f6cb19c70f3
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-10-31 16:35:44 +0100

    authenticating user

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
-import { ReactNode, useContext, useState } from "react";
-// import { AuthContext } from "../context/AuthContext";
+import { ReactNode, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 // import { isUserAuth } from "../utilities/isUserAuth.js";
 import { useIsAuth } from "../hooks/useIsAuth.js";
 
@@ -8,10 +8,20 @@ interface ProtectedRouteProp {
 }
 
 function ProtectedRoute({ children }: ProtectedRouteProp) {
-
+  const { isLoading, isLoggedIn } = useContext(AuthContext);
   const allowAccess = useIsAuth();
 
-  return <>{allowAccess ? children : <h1>not allowed here</h1>}</>;
+  return (
+    <>
+      {isLoading ? (
+        <h1>...is Loading</h1>
+      ) : allowAccess ? (
+        children
+      ) : (
+        <h1>not allowed here</h1>
+      )}
+    </>
+  );
 }
 
 export default ProtectedRoute;
