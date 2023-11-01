ebd0c0663050e65be406cbcb1f486e3e145367f4
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-01 21:12:24 +0100

    Update SignUp.tsx

--- client/src/views/SignUp.tsx
+++ client/src/views/SignUp.tsx
@@ -1,94 +1,16 @@
-import {
-  createRoutesFromElements,
-  createBrowserRouter,
-  Route,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
-import "../src/styles/main.css";
-import Footer from "./components/Footer";
-import SignUp from "./views/SignUp";
-import LogIn from "./views/LogIn";
-import About from "./views/About";
-import Home from "./views/Home";
-import NavBar from "./components/NavBar";
-import Experiences from "./views/Experiences";
-import Map from "./views/Map";
-import ErrorPage from "./views/ErrorPage";
-import ExperienceDetails from "./views/ExperienceDetails";
-import Wildlife from "./views/ExperienceTypes/Wildlife";
-import Hiking from "./views/ExperienceTypes/Hiking";
-import ExpLayout from "./components/ExpLayout";
-import Profile from "./views/Profile";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Submit from "./views/Submit";
-import { ExperiencesContextProvider } from "./context/ExperiencesContext";
-import { AuthContextProvider } from "./context/AuthContext";
-import ExperienceWithCHook from "./components/ExperienceWithCHook";
+import { useEffect } from "react";
+import Signup from "../components/Signup";
 
-function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
-        <Route index element={<Home />} />
-        <Route path="experiences" element={<ExpLayout />}>
-          <Route index element={<Experiences />} />
-          <Route path="all" element={<Experiences />} />
-          <Route path="hiking" element={<Hiking />} />
-          <Route path="wildlife" element={<Wildlife />} />
-          //! Do the same for all adventure types + make sure they're different,
-        </Route>
-        <Route
-          path="experiences/:experienceTitle"
-          element={<ExperienceDetails />}
-        />
-        <Route path="custom" element={<ExperienceWithCHook />} />
-
-        <Route path="map" element={<Map />} />
-        <Route path="about" element={<About />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="login" element={<LogIn />} />
-        <Route
-          path="profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="submit"
-          element={
-            <ProtectedRoute>
-              <Submit />
-            </ProtectedRoute>
-          }
-        />
-      </Route>
-    )
-  );
-
+function SignUp() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
-    <>
-      <div className="mainBody">
-        <AuthContextProvider>
-          <ExperiencesContextProvider>
-            <RouterProvider router={router} />
-          </ExperiencesContextProvider>
-          <Footer />
-        </AuthContextProvider>
-      </div>
-    </>
+    <div>
+      <h1>sign up</h1>
+      <Signup />
+    </div>
   );
 }
 
-const Root = () => {
-  return (
-    <>
-      <NavBar />
-      <Outlet />
-    </>
-  );
-};
-
-export default App;
+export default SignUp;
