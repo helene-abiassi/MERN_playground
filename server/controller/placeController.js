d516d568da9065df1ead28caf5af1991954477dd
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-01 21:15:42 +0100

    Update placeController.js

--- server/controller/placeController.js
+++ server/controller/placeController.js
@@ -2,18 +2,19 @@ import placeModel from "../models/placeModel.js";
 
 const getAllPlaces = async (req, res) => {
   const allPlaces = await placeModel.find();
-  console.log("allPlaces :>> ", allPlaces); //TODO -  Add the likes logic to /all places function as well
+  console.log("allPlaces :>> ", allPlaces);
+  //TODO -  Add the likes logic to /all places function as well
   res.json({
     number: allPlaces.length,
     data: allPlaces,
   });
 };
 
-//Setting up my URL Parameters
+//Setting up my URL Params
 const getPlacesByName = async (req, res) => {
   const name = req.params.name;
 
-  const { likes } = req.query;
+  const { likes } = req.query; //Setting up Query Params
 
   if (likes) {
     try {
@@ -31,7 +32,7 @@ const getPlacesByName = async (req, res) => {
     //
   } else {
     try {
-      const placeNames = await placeModel.find({ name: name }); //RAULNOTE - Params and values are case sensitive?
+      const placeNames = await placeModel.find({ name: name });
 
       if (placeNames.length > 0) {
         res.status(200).json({
@@ -42,7 +43,7 @@ const getPlacesByName = async (req, res) => {
         res.status(200).json({
           number: placeNames.length,
           errorMessage: `Sorry no places with the name ${name} exist.`,
-        }); //TODO Review which error code to use / when (here and below)
+        });
       }
     } catch (error) {
       console.log("placeNames error :>> ", error);
@@ -52,3 +53,48 @@ const getPlacesByName = async (req, res) => {
 };
 
 export { getAllPlaces, getPlacesByName };
+//TODO Review which error code to use / when (here and below)
+
+const getCitiesByCountryCode = async (req, res) => {
+  // console.log("req :>> ".bgMagenta, req);
+  // const countryCode = req.params.countryCode;
+  const { countryCode } = req.params;
+  const { likes } = req.query;
+
+  if (likes) {
+    try {
+      const cities = await cityModel.find({
+        countryCode: countryCode,
+        likes: { $gte: likes },
+      });
+      res.status(200).json({
+        number: cities.length,
+        cities,
+      });
+    } catch (error) {}
+
+    //do stuff using likes
+  } else {
+    //all of the stuff below happens when there are no "likes" sent from the client.
+    try {
+      const cities = await cityModel.find({ countryCode: countryCode });
+      if (cities.length > 0) {
+        res.status(200).json({
+          number: cities.length,
+          cities,
+        });
+      } else {
+        res.status(200).json({
+          number: cities.length,
+          message: `sorry no cities with  country code ${countryCode}`,
+        });
+      }
+    } catch (error) {
+      console.log("error :>> ", error);
+      res.status(500).json({
+        errorMessage: "something went wrong in the request",
+        error,
+      });
+    }
+  }
+};
