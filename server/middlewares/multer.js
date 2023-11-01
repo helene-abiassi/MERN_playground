c2e365bbcfd21f5e93b54c4b454e0e2d4cc0812a
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-01 21:17:00 +0100

    Update multer.js

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -4,7 +4,6 @@ import path from "path";
 const storage = multer.diskStorage({});
 
 const fileFilter = (req, file, cb) => {
-  // do we accept the file the user is uploading or not, type of file
   console.log("req>>>", req);
   console.log("file :>> ", file);
 
