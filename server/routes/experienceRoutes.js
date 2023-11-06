499204e3506666d34ad0470a7d4c2227e0ed0bdf
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-06 12:12:28 +0100

    Update experienceRoutes.js

--- server/routes/experienceRoutes.js
+++ server/routes/experienceRoutes.js
@@ -1,15 +1,20 @@
 import express from "express";
 import {
+  deleteComment,
+  deleteExperience,
   getAllExperiences,
   getExperiencesByCity,
   getExperiencesByCountry,
   getExperiencesById,
   getExperiencesByType,
+  submitComment,
   submitExperience,
   uploadMultiplePhotos,
   uploadPhoto,
 } from "../controller/experienceController.js";
 import { multerUpload } from "../middlewares/multer.js";
+
+import jwtAuth from "../middlewares/jwtAuth.js";
 
 const router = express.Router();
 
@@ -17,16 +22,18 @@ const router = express.Router();
 router.get("/all", getAllExperiences);
 router.get("/id/:_id", getExperiencesById);
 router.get("/type/:experienceType", getExperiencesByType);
-// router.get("/:country/:city", getExperiencesByCity);
 router.get("/country/:country", getExperiencesByCountry);
 
 //POST Routes
 router.post("/experiencesubmission", submitExperience);
+router.post("/experiences/:_id/comments", jwtAuth, submitComment);
 router.post("/mainphotoupload", multerUpload.single("photo"), uploadPhoto);
 router.post(
   "/photoalbumupload",
   multerUpload.array("photo_body", 4),
   uploadMultiplePhotos
 );
+router.delete("/deletecomment/:_id", jwtAuth, deleteComment);
+router.delete("/deleteexperience/:_id", jwtAuth, deleteExperience);
 
 export default router;
