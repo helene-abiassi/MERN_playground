c6f4edddcddf91a6df650f6fe7b1843e74b92a67
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-07 23:12:50 +0100

    Update ProfileCard.tsx

--- client/src/components/ProfileCard.tsx
+++ client/src/components/ProfileCard.tsx
@@ -8,58 +8,24 @@ function ProfileCard() {
     useContext(AuthContext);
   const navigateTo = useNavigate();
 
-  // const getProfile = async () => {
-  //   const token = localStorage.getItem("token");
-
-  //   if (!token) {
-  //     alert("you need to log in first!");
-  //     //Redirect user to login/ protected route page
-  //   }
-  //   if (token) {
-  //     const myHeaders = new Headers();
-  //     myHeaders.append("Authorization", `Bearer ${token}`);
-
-  //     const requestOptions = {
-  //       method: "GET",
-  //       headers: myHeaders,
-  //     };
-
-  //     try {
-  //       const response = await fetch(
-  //         "http://localhost:5005/api/users/profile",
-  //         requestOptions
-  //       );
-
-  //       if (!response.ok) {
-  //         alert(response.statusText);
-  //       }
-
-  //       if (response.ok) {
-  //         const result = await response.json();
-  //         console.log("result to get my user profile :>> ", result);
-  //         const user = result.userProfile as User;
-
-  //         setUser(user);
-  //       }
-  //     } catch (err) {
-  //       const error = err as Error;
-  //       console.log("error :>> ", error.message);
-  //     }
-  //   }
-  // };
-
   const handleDeleteProfile = (userID: string) => {
     deleteProfile(userID);
     navigateTo("/");
   };
 
+  const updateProfile = () => {
+    // do whaterever to update (fetch)
+    //when you get the result (ok result) -> getProfile()
+  };
   const handleLogOut = () => {
     logOut();
     navigateTo("/");
   };
   console.log("user in PROFILE :>> ", user);
 
-  useEffect(() => {}, [getProfile]);
+  useEffect(() => {
+    getProfile();
+  }, []);
 
   return (
     <div>
