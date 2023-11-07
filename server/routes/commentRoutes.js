3618eb75aee68b40d5344a28f1d774c16d541cac
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-07 23:20:10 +0100

    Update commentRoutes.js

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -3,8 +3,11 @@ import {
   getAllComments,
   getCommentsByExperienceId,
   getCommentsByUserId,
+  testFormRequest,
 } from "../controller/commentController.js";
 import jwtAuth from "../middlewares/jwtAuth.js";
+import multer from "multer";
+import { multerUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
@@ -12,5 +15,7 @@ const router = express.Router();
 router.get("/all", getAllComments);
 router.get("/user/:author", getCommentsByUserId);
 router.get("/experience/:experience", getCommentsByExperienceId);
+//RAULNOTE - route to test how post request with form-data works
+// router.post("/test", multerUpload.single("image"), testFormRequest);
 
 export default router;
