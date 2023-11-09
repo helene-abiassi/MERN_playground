c9ce7717c91bc6383f77f666ed6c1101c9144f4f
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-10 00:16:57 +0100

    Update ExpCard.tsx

--- client/src/components/ExpCard.tsx
+++ client/src/components/ExpCard.tsx
@@ -1,18 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Experience } from "../types/customTypes";
 import { useContext, useEffect } from "react";
 import { formatDate } from "./Functions";
 import { AuthContext } from "../context/AuthContext";
+import { ExperiencesContext } from "../context/ExperiencesContext";
 
 export interface ExperienceCardProp {
   experience: Experience | Experience[];
 }
 
 function ExpCards({ experience }: ExperienceCardProp) {
-  const { author, title, caption, publication_date, photo, bookmarked_by } =
-    experience as Experience;
+  const {
+    _id,
+    author,
+    title,
+    caption,
+    publication_date,
+    photo,
+    bookmarked_by,
+  } = experience as Experience;
 
   const { user } = useContext(AuthContext);
+  const { deleteExperience, bookmarkExperience } =
+    useContext(ExperiencesContext);
+
+  const navigateTo = useNavigate();
+
+  const handleDeleteExperience = async (experienceID: string) => {
+    deleteExperience(experienceID);
+    navigateTo("/experiences");
+  };
+
+  const handleBookmarkExperience = async (experienceID: string) => {
+    bookmarkExperience(experienceID);
+    alert("Added to bookmarks!"); //!Replace with toast or just color change
+  };
 
   useEffect(() => {}, [user, experience]);
 
@@ -20,22 +42,44 @@ function ExpCards({ experience }: ExperienceCardProp) {
     <div>
       <div className="storyCards">
         <div className="storyCardSignle">
-          <h2>{title}</h2>
-          <div>
-            <img className="gridPhoto" src={photo} />
+          <div className="textBox">
+            <button
+              onClick={() => {
+                handleBookmarkExperience(_id);
+              }}
+            >
+              Bookmark
+            </button>
+            {user?.email === experience.author.email && (
+              <button
+                onClick={() => {
+                  handleDeleteExperience(_id);
+                }}
+              >
+                Delete
+              </button>
+            )}{" "}
+            {user?.email === experience.author.email && (
+              <Link style={{ backgroundColor: "black" }} to={`/update/${_id}`}>
+                Edit
+              </Link>
+            )}{" "}
+            <h2>{title}</h2>
+            <div>
+              <img className="gridPhoto" src={photo} />
+            </div>
+            <p>Bookmarks: {experience.bookmarked_by.length}</p>
+            <p className="storyCardCaption">{caption}</p>
+            <p>
+              written by {author?.username}, {formatDate(publication_date)}
+            </p>
+            <Link to={`${title}`} state={{ experience: experience }}>
+              View More!!
+            </Link>
           </div>
-          <p>Bookmarks: {experience.bookmarked_by.length}</p>
-
-          <p className="storyCardCaption">{caption}</p>
-          <p>
-            written by {author?.username}, {formatDate(publication_date)}
-          </p>
-          <Link to={`${title}`} state={{ experience: experience }}>
-            View More!!
-          </Link>
         </div>
-      </div>
-      <br />
+        <br />
+      </div>{" "}
     </div>
   );
 }
