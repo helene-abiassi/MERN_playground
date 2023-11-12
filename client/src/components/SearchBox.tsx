a9eb157e5089c1196edae1a6ac92305fe215ad8c
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-12 19:26:50 +0100

    search by city working

--- client/src/components/SearchBox.tsx
+++ client/src/components/SearchBox.tsx
@@ -1,37 +1,67 @@
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 export interface SearchBoxProps {
-  onSearch: (criteria: string) => void;
+  onCriteriaSearch: (criteria: string) => void;
+  onCitySearch: (city: string) => void;
 }
 
-function SearchBox({ onSearch }: SearchBoxProps) {
-  const handleSearch = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const criteria = event.target.value;
-    onSearch(criteria);
+function SearchBox({ onCriteriaSearch, onCitySearch }: SearchBoxProps) {
+  const [searchCriteria, setSearchCriteria] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [searchCityInput, setSearchCityInput] = useState("");
+
+  const handleCriteriaChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const criteria = e.target.value;
+    setSearchCriteria(criteria);
+    onCriteriaSearch(criteria);
     if (criteria === "Sort By") {
-      onSearch("");
+      setSearchCriteria("");
+      onCriteriaSearch("");
     }
   };
 
-  useEffect(() => {}, []);
+  const handleCityChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const normalizedQuery = e.target.value.toLowerCase();
+    setSearchCityInput(normalizedQuery);
+  };
+
+  const handleSearchClick = () => {
+    setSearchCity(searchCityInput);
+
+    onCitySearch(searchCityInput);
+  };
+
+  useEffect(() => {
+    // onCriteriaSearch("")
+    // handleSearchClick();
+  }, [searchCityInput]);
   return (
     <div className="mainSearchBox">
       <div className="searchBox">
         <div className="searchByCombo">
-          <div className="searchByIngBox"></div>
-          <select onChange={handleSearch} defaultValue="Sort By">
-            <option value={"Sort By"}>Sort By</option>
+          <div className="searchByIngBox">
+            <input
+              type="text"
+              placeholder="Search by city..."
+              value={searchCityInput}
+              onChange={handleCityChange}
+            />
+          </div>
+          <select onChange={handleCriteriaChange}>
+            <option value={""}>Sort By</option>
             <option value={"Most Bookmarked"}>Most Bookmarked</option>
             <option value={"Newest"}>Newest</option>
             <option value={"Oldest"}>Oldest</option>
             <option value={"Most Commented"}>Most Commented</option>
-            {/*Add title/ city/ country?*/}
           </select>
-          <button>Reset</button>
+          <button className="searchButton" onClick={handleSearchClick}>
+            Search
+          </button>
         </div>
       </div>
     </div>
   );
+
 }
 
 export default SearchBox;
