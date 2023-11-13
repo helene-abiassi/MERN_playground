0fc3ca57937d1223e5d67fd3deefb0aba9737b26
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-13 01:22:15 +0100

    Update Home.tsx

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,6 +1,6 @@
 import "../styles/Home.css";
 import { Link } from "react-router-dom";
-import Carousel from "../components/Carousel";
+// import Carousel from "../components/Carousel";
 import { useContext, useEffect } from "react";
 import { ExperiencesContext } from "../context/ExperiencesContext";
 import { AuthContext } from "../context/AuthContext";
@@ -24,16 +24,23 @@ function Home() {
       >
         <div className="beigeBox">
           <h2>Welcome {user?.username}!</h2>
-          <h3 className="subHeader">
-            {experiences.length} experiences for you to discover
-          </h3>
+          <h3>{experiences?.length} experiences for you to discover</h3>
           <p>
-            Lorem ipsibunventore, amet natus reiciendis ipsam quasi fugiat at ut
-            soluta consequatur et magnam illum corporis. Quaerat blanditiis
-            voluptatem, adipisci numquam autem at.
+            Discovers voyage! A public travel blog, where people share some of
+            the unique experiences and activities they came up with while on
+            vacation. Whether you went on an alchemy walk in Prague, listened to
+            live Faro music at a miradouro in Portugal, or discovered a hiddne
+            gem during your travels, share your story with us.
           </p>
+          <figcaption>
+            exclusive access only for responsible tourists.
+          </figcaption>
           <br />
-          <Link className="nakdButton" to={"/experiences"}>
+          <Link
+            style={{ color: "black" }}
+            className="nakdButton"
+            to={"/experiences"}
+          >
             start exploring
           </Link>
         </div>
