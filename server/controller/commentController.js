f113cdd748dba2de3a8336c9ed4cdda9c80af7f1
Helene Abi Assi <140400288+helene-abiassi@users.noreply.github.com>
2023-11-28 22:59:29 +0100

    Update commentController.js

--- server/controller/commentController.js
+++ server/controller/commentController.js
@@ -71,10 +71,6 @@ const getCommentsByExperienceId = async (req, res) => {
     });
   }
 };
-const testFormRequest = async (req, res) => {
-  console.log("req.body :>> ", req.body);
-  console.log("req :>> ", req);
-};
 
 export {
   getAllComments,
